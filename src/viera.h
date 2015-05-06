28f4b97826a439c80972ea72f450d9016229ff83
Stamincx <staminchia@tin.it>
2015-05-06 23:07:52 +0200

    Update viera.h

--- src/viera.h
+++ src/viera.h
@@ -1,1 +1,9 @@
+#pragma once
 
+#include <pebble.h>
+
+void viera_init(void);
+void viera_deinit(void);
+void viera_in_received_handler(DictionaryIterator *iter);
+void viera_reload_data_and_mark_dirty();
+void viera_request(uint8_t request);
